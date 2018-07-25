5c154660b535fa5edb398301829481c8c9bd7d5b
Michael Fynes <mikefynes@gmail.com>
2018-07-25 21:38:25 +0300

    setting up sliding window logic

--- src/sliding_window.go
+++ src/sliding_window.go
@@ -1,3 +1,22 @@
 package main
 
 
+type SlidingWindow struct{
+	times []int
+	values []int
+	size int
+	diff int
+	isClosed bool
+}
+
+func (w *SlidingWindow) addPoint(time, value int){
+
+	if  len(w.values) < w.size {
+		w.times = append(w.times, time)
+		w.values = append(w.values, value)
+	}
+}
+
+func (w *SlidingWindow) wouldClose(time int){
+
+}
