6546047a00a34542653cfe21ef017dde31934895
Michael Fynes <mikefynes@gmail.com>
2018-07-26 21:05:54 +0300

    added final tests

--- src/csv_util_test.go
+++ src/csv_util_test.go
@@ -2,6 +2,14 @@ package main
 
 import "testing"
 
-func TestCsvReader(t *testing.T){
+func TestCsvReaderDoc1(t *testing.T){
 	readCsvWithSlidingWindow("Round 1. Software engineering test cases - test2.csv", 5, 25000)
+}
+
+func TestCsvReaderDoc2(t *testing.T){
+	readCsvWithSlidingWindow("Round 1. Software engineering test cases - test3.csv", 5, 25000)
+}
+
+func TestCsvReaderDoc3(t *testing.T){
+	readCsvWithSlidingWindow("Round 1. Software engineering test cases - test4.csv", 5, 25000)
 }
