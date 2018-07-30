8af6df8e6d2e63f2ae4c67e1e6d747e1c3a5112a
Michael Fynes <mikefynes@gmail.com>
2018-07-30 17:26:45 +0300

    remove redundant code

--- src/sliding_window.go
+++ src/sliding_window.go
@@ -51,13 +51,3 @@ func (w *SlidingWindow) getMedian()(median int){
 
 	return
 }
-
-func reverseIntArray(s []int)(reversed []int){
-	for i, j := 0, len(s)-1; i < j; i, j = i+1, j-1 {
-		s[i], s[j] = s[j], s[i]
-	}
-
-	reversed = s
-
-	return
-}
