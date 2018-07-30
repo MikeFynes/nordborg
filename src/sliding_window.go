e1ec52a1075881f7609d7d18ef78b73e900de16a
Michael Fynes <mikefynes@gmail.com>
2018-07-30 18:11:35 +0300

    initial attempt at implementing a quick median solution

--- src/sliding_window.go
+++ src/sliding_window.go
@@ -39,10 +39,6 @@ func (w *SlidingWindow) getMedian()(median int){
 	switch{
 	case len(values) == 1 : median = -1
 	case len(values) % 2 == 0 :
-		// Complexity
-		// x1 = len(values) / 2  -1 		2
-		// x2 =  len(values) / 2 +1 -1 		3
-		// Complexity is currently 5
 		var itemOne = len(values) / 2 -1
 		var itemTwo = len(values) / 2
 		var interimOne = values[itemOne]
@@ -54,4 +50,27 @@ func (w *SlidingWindow) getMedian()(median int){
 	}
 
 	return
+}
+
+
+// attempt at QuickMedian algorithm see : https://www.i-programmer.info/babbages-bag/505-quick-median.html
+func(w *SlidingWindow) getMedianQuickMedian()(median int){
+	var values =  make([]int, len(w.values))
+	copy(values, w.values)
+	var k = len(values) / 2
+	var n = len(w.values)
+	var i = 0
+	var j = 0
+	var L = 0
+	var R = n -1
+
+	for L < R {
+		x := values[k]
+		i = L
+		j = R
+}
+
+
+
+
 }
